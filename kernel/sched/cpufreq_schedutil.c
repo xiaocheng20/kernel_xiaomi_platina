7847c86d834900c539de0165aa69ab0aaa246caa
idkwhoiam322 <snkm1999@gmail.com>
2018-12-18 15:12:50 +0300

    set proper schedutil rates
    Signed-off-by: nysascape <jago@nysascape.digital>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -817,8 +817,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	} else {
 		unsigned int lat;
 
-                tunables->up_rate_limit_us = UP_RATE_LIMIT_US;
-                tunables->down_rate_limit_us = DOWN_RATE_LIMIT_US;
+		tunables->up_rate_limit_us = 500;
+		tunables->down_rate_limit_us = 20000;
 		lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 		if (lat) {
                         tunables->up_rate_limit_us *= lat;
